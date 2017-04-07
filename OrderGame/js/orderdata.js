3d0dc5f95ca87963ca6e7ba897f6c4cb1265507c
balaji <balaji@e6eec28d-ecfc-2d4f-a4d2-7984f2a0a39b>
2017-04-07 13:17:10 +0000

    OrderGame

---
+++ OrderGame/js/orderdata.js
@@ -0,0 +1,67 @@
+﻿var orderGame = [
+	{
+	    "GameType": "Num",
+	    "PID": "L1",
+	    "PMame": "Ordering No 1 to 5",
+	    "Wlist":
+         [
+            { "ID": "1", "ImgUrl": "1.png" },
+            { "ID": "2", "ImgUrl": "2.png" },
+            { "ID": "3", "ImgUrl": "3.png" },
+            { "ID": "4", "ImgUrl": "4.png" },
+            { "ID": "5", "ImgUrl": "5.png" }
+         ]
+	},
+	{
+	    "GameType": "Num",
+	    "PID": "L2",
+	    "PMame": "Ordering No 6 to 10",
+	    "Wlist":
+        [
+           { "ID": "6", "ImgUrl": "6.png" },
+           { "ID": "7", "ImgUrl": "7.png" },
+           { "ID": "8", "ImgUrl": "8.png" },
+           { "ID": "9", "ImgUrl": "9.png" },
+           { "ID": "10", "ImgUrl": "10.png" }
+        ]
+	},
+	{
+	    "GameType": "Num",
+	    "PID": "L3",
+	    "PMame": "Ordering No 11 to 15",
+	    "Wlist":
+        [
+           { "ID": "11", "ImgUrl": "11.png" },
+           { "ID": "12", "ImgUrl": "12.png" },
+           { "ID": "13", "ImgUrl": "13.png" },
+           { "ID": "14", "ImgUrl": "14.png" },
+           { "ID": "15", "ImgUrl": "15.png" }
+        ]
+	},
+	{
+	    "GameType": "Num",
+	    "PID": "L4",
+	    "PMame": "Ordering No 16 to 20",
+	    "Wlist":
+        [
+           { "ID": "16", "ImgUrl": "16.png" },
+           { "ID": "17", "ImgUrl": "17.png" },
+           { "ID": "18", "ImgUrl": "18.png" },
+           { "ID": "19", "ImgUrl": "19.png" },
+           { "ID": "20", "ImgUrl": "20.png" }
+        ]
+	},
+	{
+	    "GameType": "alpha",
+	    "PID": "L4",
+	    "PMame": "Ordering No 16 to 20",
+	    "Wlist":
+        [
+           { "ID": "1", "ImgUrl": "a.png" },
+           { "ID": "2", "ImgUrl": "b.png" },
+           { "ID": "3", "ImgUrl": "c.png" },
+           { "ID": "4", "ImgUrl": "d.png" },
+           { "ID": "5", "ImgUrl": "e.png" }
+        ]
+	}
+];
